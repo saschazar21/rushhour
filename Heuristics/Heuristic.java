ffe40a37d3d45651fcc16996297a7b3fa66bd5ae
Sascha Zarhuber <sascha.zarhuber@students.fh-hagenberg.at>
2016-05-02 13:02:33 +0200

    Reformatted code

--- Heuristics/Heuristic.java
+++ Heuristics/Heuristic.java
@@ -3,13 +3,12 @@ package Heuristics;
 import AStar.State;
 
 /**
- * This is the interface that all heuristics must implement.  In
- * particular, all such implementations must include a method
- * <tt>getValue</tt> that returns the value of the heuristic function
- * at any given state.
+ * This is the interface that all heuristics must implement. In particular, all
+ * such implementations must include a method <tt>getValue</tt> that returns the
+ * value of the heuristic function at any given state.
  */
 public interface Heuristic {
 
-    /** Returns the value of the heuristic function at the given state. */
-    public int getValue(State state);
+	/** Returns the value of the heuristic function at the given state. */
+	public int getValue(State state);
 }
