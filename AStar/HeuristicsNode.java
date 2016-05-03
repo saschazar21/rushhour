c32c9692afaf10b20025bbafdc11b1f4f4cfc31e
Sascha Zarhuber <sascha.zarhuber@students.fh-hagenberg.at>
2016-05-03 13:13:42 +0200

    Refactored constructor of HeuristicsNode

--- AStar/HeuristicsNode.java
+++ AStar/HeuristicsNode.java
@@ -2,10 +2,26 @@ package AStar;
 
 public class HeuristicsNode extends Node {
 	
-	private float f;
-	private float g;
-	private float heuristic;
+	private int f;
+	private int g;
+	private int heuristic;
 	
+	/**
+	 * A wrapper for the node class enhanced by heuristics.
+	 * 
+	 * @param state: The state that resides in this node
+	 * @param depth: The current depth of this node
+	 * @param parent: The node's parent, otherwise null if root
+	 * @param gValue: The path cost already paid for reaching this node
+	 * @param heuristic: The estimated path cost from this node to goal
+	 */
+	public HeuristicsNode(State state, int depth, Node parent, int gValue, int heuristic) {
+		super(state, depth, parent);
+		this.f = gValue + heuristic;
+		this.g = gValue;
+		this.heuristic = heuristic;
+	}
+
 	/**
 	 * A wrapper for the node class enhanced by heuristics.
 	 * 
@@ -13,11 +29,8 @@ public class HeuristicsNode extends Node {
 	 * @param gValue: The path cost already paid for reaching this node
 	 * @param heuristics: The estimated path cost from this node to goal
 	 */
-	public HeuristicsNode(Node node, float gValue, float heuristic) {
-		super(node.getState(), node.getDepth(), node.getParent());
-		this.f = gValue + heuristic;
-		this.g = gValue;
-		this.heuristic = heuristic;
+	public HeuristicsNode(Node node, int gValue, int heuristic) {
+		this(node.getState(), node.getDepth(), node.getParent(), gValue, heuristic);
 	}
 	
 	/**
@@ -26,14 +39,14 @@ public class HeuristicsNode extends Node {
 	 * @param node: The current node
 	 */
 	public HeuristicsNode(Node node) {
-		this(node, 0f, 0f);
+		this(node, 0, 0);
 	}
 
 	/**
 	 * Getter for the gValue.
 	 * @return The already paid path cost for reaching this node
 	 */
-	public float getG() {
+	public int getG() {
 		return g;
 	}
 
@@ -41,7 +54,7 @@ public class HeuristicsNode extends Node {
 	 * Setter for the gValue
 	 * @param g: Sets the path cost to reach this node
 	 */
-	public void setG(float g) {
+	public void setG(int g) {
 		this.g = g;
 		this.f = g + this.heuristic;
 	}
@@ -50,7 +63,7 @@ public class HeuristicsNode extends Node {
 	 * Getter for the heuristic value.
 	 * @return The estimated path cost from this node to goal
 	 */
-	public float getHeuristic() {
+	public int getHeuristic() {
 		return heuristic;
 	}
 
@@ -58,7 +71,7 @@ public class HeuristicsNode extends Node {
 	 * Setter for the heuristic value.
 	 * @param heuristic: Sets the estimated path cost for reaching the goal from this node
 	 */
-	public void setHeuristic(float heuristic) {
+	public void setHeuristic(int heuristic) {
 		this.heuristic = heuristic;
 		this.f = this.g + heuristic;
 	}
@@ -68,7 +81,7 @@ public class HeuristicsNode extends Node {
 	 * @return The total amount of costs paid to reach this node + estimated costs 
 	 * to reach goal from this node.
 	 */
-	public float getF() {
+	public int getF() {
 		return f;
 	}
 }
