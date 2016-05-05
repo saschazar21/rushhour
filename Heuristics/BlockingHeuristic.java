5d9db4dfae8add357f3c00fc96ed19bdb97be99a
Sascha Zarhuber <sascha.zarhuber@students.fh-hagenberg.at>
2016-05-05 15:29:38 +0200

    + Added BlockingHeuristic

--- Heuristics/BlockingHeuristic.java
+++ Heuristics/BlockingHeuristic.java
@@ -11,14 +11,18 @@ import AStar.State;
  * implemented by filling in the constructor and the <tt>getValue</tt> method.
  */
 public class BlockingHeuristic implements Heuristic {
+	
+	private Puzzle puzzle;		// The current puzzle to evaluate heuristics on
+	private int carPosFixed;	// The initial fixed position of our car
+	private int blocking;		// No of blocking cars in the way of our car
 
 	/**
 	 * This is the required constructor, which must be of the given form.
 	 */
 	public BlockingHeuristic(Puzzle puzzle) {
-
-		// your code here
-
+		
+		this.puzzle = puzzle;							// Store current puzzle
+		this.carPosFixed = puzzle.getFixedPosition(0);	// Get fixed position of our car.
 	}
 
 	/**
@@ -26,10 +30,44 @@ public class BlockingHeuristic implements Heuristic {
 	 * state.
 	 */
 	public int getValue(State state) {
-
-		// your code here
-		return 0;
-
+		
+		// Return 0, if state equals goal state (No further heuristics needed)
+		if (state.isGoal()) {
+			return 0;
+		}
+		
+		this.blocking = 1;	// At least one car is still blocking our way.
+		
+		// Calculate the outermost position of our car,
+		// -1 needed due to variable position not being identified as 0 otherwise
+		int carPosFront = state.getVariablePosition(0) + (this.puzzle.getCarSize(0) - 1);
+		
+		int numCars = this.puzzle.getNumCars();
+		for (int i = 0; i < numCars; i++) {
+			
+			if (!this.puzzle.getCarOrient(i)) {			// Car is horizontally aligned as well,
+				continue;								// does not block our car.
+			}
+			
+			if (this.puzzle.getFixedPosition(i) <= carPosFront) {
+				continue;								// Car is behind of our car, does not block.
+			}
+			
+			/* Now calculate how many cars are really standing between us and the goal, idea:
+			* -----------------
+			* 
+			* Check on behalf of our car's point of view, if possibly blocking car reaches from our
+			* right (variablePosition of i smaller than our fixedPosition) to our 
+			* left (variablePosition + length of i greater than our fixedPosition),
+			* as we're looking left to the top of the game board, right to the bottom.
+			*/
+			int currentCarPos = state.getVariablePosition(i);
+			int currentCarPosFront = currentCarPos + this.puzzle.getCarSize(i);
+			if (currentCarPosFront >= this.carPosFixed && currentCarPos <= this.carPosFixed) {
+				this.blocking++;
+			}
+		}
+		return this.blocking;
 	}
 
 }
